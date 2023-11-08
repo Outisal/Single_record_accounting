7749421ae41ac0b933e1831e088e2ee79ea585b4
Outi Salonen <147878561+Outisal@users.noreply.github.com>
2023-11-08 03:11:01 +0200

    Add routes and templates for login and registering

---
+++ routes.py
@@ -0,0 +1,40 @@
+from app import app
+import users
+from flask import render_template, request, redirect
+
+
+@app.route("/")
+def test():
+    return "Welcome"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Incorrect username or password")
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="passwords don't match")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Registeration failed")
