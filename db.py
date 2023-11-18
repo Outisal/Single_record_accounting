45c2064d76461a967f796f02a4c1d12e0d9152e4
Outi Salonen <147878561+Outisal@users.noreply.github.com>
2023-11-18 10:09:56 +0200

    Refactor order of imports

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
-from app import app
-from flask_sqlalchemy import SQLAlchemy
 from os import getenv
+from flask_sqlalchemy import SQLAlchemy
+from app import app
 
 db_url = getenv("DATABASE_URL")
 app.config["SQLALCHEMY_DATABASE_URI"] = db_url
